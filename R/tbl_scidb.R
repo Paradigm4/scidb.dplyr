4c586ecda790f2326c38359962824a520dbcd4f7
Bryan W Lewis <blewis@paradigm4.com>
2017-05-09 11:26:35 -0400

    Basic summarise/group_by prototype
    WARNING: SciDB-specific aggregate syntax is required (thus, for instance, mean needs to be written 'avg', etc.)

--- R/tbl_scidb.R
+++ R/tbl_scidb.R
@@ -30,6 +30,7 @@ dim.tbl_scidb <- function(x) {
 
 #' @export
 print.tbl_scidb <- function(x, ...) {
+  if("groups" %in% names(attributes(x))) message("Groups: ", paste(attr(x, "groups"), collapse=", "))
   print(x$db, ...)
 }
 
@@ -54,13 +55,6 @@ as_data.frame.tbl_scidb <- function(x, ...) {
 #' @export
 as.tbl_scidb <- function(x, db, ...) as.scidb(db, x, ...)
 
-# internal
-call2str <- function(.x)
-{
-# XXX fragile, improve this, also remove ::: call
-  if("call" %in% class(.x$expr)) return(rsub(capture.output(.x$expr), .x$env))
-  rsub(as.character(.x$expr), .x$env)
-}
 
 # Verbs -----------------------------------------------------------------------
 
@@ -83,7 +77,6 @@ select_.tbl_scidb <- function(.data, ..., .dots = list()) {
   tbl(scidb(.data$db@meta$db, sprintf("project(%s, %s)", .data$db@name, paste(vars, collapse=","))))
 }
 
-
 #' @export
 filter_.tbl_scidb <- function(.data, ..., .dots = list()) {
   dots <- all_dots(.dots, ...)
@@ -100,40 +93,6 @@ filter_.tbl_scidb <- function(.data, ..., .dots = list()) {
   tbl(scidb(.data$db@meta$db, expr))
 }
 
-# AFL expression syntax differences XXX not finished -- these substitutions must be supressed in quoted strings!
-# internal function
-aflify <- function(expr)
-{
-  expr = gsub("%as%", " as ", expr)
-  expr = gsub("==", " = ", expr)
-  expr = gsub("!=", " <> ", expr)
-  expr = gsub("\\|", " or ", expr)
-  expr = gsub("\\|\\|", " or ", expr)
-  expr = gsub('"', "'", expr)
-  expr
-}
-
-# internal, handle R() escaped values
-rsub <- function (x, env)
-{   
-    x = sprintf(" %s", x)
-    if (!grepl("[^[:alnum:]_]R\\(",x))
-        return(x)
-    imbalance_paren = function(x) {
-        which(cumsum((as.numeric(charToRaw(x) == charToRaw("("))) -
-            (as.numeric(charToRaw(x) == charToRaw(")")))) < 0)[1]
-    }
-    y = gsub("([^[:alnum:]_])R\\(", "\\1@R(", x)
-    y = strsplit(y, "@R\\(")[[1]]
-    expr = Map(function(x) {
-        i = imbalance_paren(x)
-        rexp = eval(parse(text = substring(x, 1, i - 1)), envir = env)
-        rmdr = substring(x, i + 1)
-        paste(rexp, rmdr, sep = "")
-    }, y[-1])
-    sprintf("%s%s", y[1], paste(expr, collapse = ""))
-}
-
 #' @export
 mutate_.tbl_scidb <- function(.data, ..., .dots = list()) {
   dots <- all_dots(.dots, ...)
@@ -147,5 +106,58 @@ mutate_.tbl_scidb <- function(.data, ..., .dots = list()) {
                }, error=function(e) call2str(.x))),
          sep=", ", collapse=", ")
   expr = aflify(sprintf("apply(%s, %s)", .data$db@name, .args))
+  tbl(scidb(.data$db@meta$db, expr))
+}
+
+#' @export
+transmute_.tbl_scidb <- function(.data, ..., .dots = list()) {
+  dots <- all_dots(.dots, ...)
+  .args = paste(names(dots),
+             lapply(dots,
+               function(.x) tryCatch({
+                   if (class(eval(.x$expr, envir=.x$env))[1] %in% "scidb")
+                   {
+                     eval(.x$expr, envir=.x$env)@name
+                   } else call2str(.x)
+               }, error=function(e) call2str(.x))),
+         sep=", ", collapse=", ")
+  expr = aflify(sprintf("project(apply(%s, %s), %s)", .data$db@name, .args, paste(names(dots), collapse=",")))
+  tbl(scidb(.data$db@meta$db, expr))
+}
+
+#' @export
+group_by_.tbl_scidb <- function(.data, ..., .dots = list()) {
+  dots <- all_dots(.dots, ...)
+  groups <- NULL
+  if(dots$add$expr) groups <- attr(.data, "groups")
+  .args = lapply(dots[-1],
+               function(.x) tryCatch({
+                   if (class(eval(.x$expr, envir=.x$env))[1] %in% "scidb")
+                   {
+                     stop("invalid context for SciDB array")
+                   } else call2str(.x)
+               }, error=function(e) call2str(.x)))
+  attr(.data, "groups") <- c(groups, gsub(" *$", "", gsub("^ *", "", unlist(.args))))
+  .data
+}
+
+#' @export
+summarise_.tbl_scidb <- function(.data, ..., .dots = list()) {
+  dots <- all_dots(.dots, ...)
+  N <- names(dots)
+  N[N == ""] <- "V"
+  N <- paste(" as ", gsub("\\.", "_", make.names(N, unique=TRUE)))
+  .args = paste(lapply(dots,
+               function(.x) tryCatch({
+                   if (class(eval(.x$expr, envir=.x$env))[1] %in% "scidb")
+                   {
+                     eval(.x$expr, envir=.x$env)@name
+                   } else call2str(.x)
+               }, error=function(e) call2str(.x), warning=function(e) call2str(.x))), N,
+         sep=" ", collapse=", ")
+  if(is.null(attr(.data, "groups")))
+    expr = aflify(sprintf("aggregate(%s, %s)", .data$db@name, paste(.args, collapse=",")))
+  else
+    expr = aflify(sprintf("grouped_aggregate(%s, %s)", .data$db@name, paste(c(attr(.data, "groups"), .args), collapse=",")))
   tbl(scidb(.data$db@meta$db, expr))
 }
